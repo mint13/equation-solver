99d97d2f8b04ed730cbdb2d874438c07feb26a6b
Mint Medillo <m.medillo13@gmail.com>
2021-09-15 19:13:49 +0800

    update handleMath to handleEquation

--- script.js
+++ script.js
@@ -21,22 +21,21 @@ let parse = (equation) => {
     const newEquation = equation.replace(PARENTHESIS_REGEX, result)
     return parse(newEquation)
   } else if (equation.match(EXPONENT_REGEX)) {
-    const result = handleMath(equation.match(EXPONENT_REGEX).groups)
+    const result = handleEquation(equation.match(EXPONENT_REGEX).groups)
     const newEquation = equation.replace(EXPONENT_REGEX, result)
     return parse(newEquation)
   } else if (equation.match(MULTIPLY_DIVIDE_REGEX)) {
-    const result = handleMath(equation.match(MULTIPLY_DIVIDE_REGEX).groups)
+    const result = handleEquation(equation.match(MULTIPLY_DIVIDE_REGEX).groups)
     const newEquation = equation.replace(MULTIPLY_DIVIDE_REGEX, result)
     return parse(newEquation)
   } else if (equation.match(ADD_SUBTRACT_REGEX)) {
-    const result = handleMath(equation.match(ADD_SUBTRACT_REGEX).groups)
+    const result = handleEquation(equation.match(ADD_SUBTRACT_REGEX).groups)
     const newEquation = equation.replace(ADD_SUBTRACT_REGEX, result)
     return parse(newEquation)
   } else {
     return parseFloat(equation)
   }
 }
-
 let handleEquation = ({ operand1, operand2, operation }) => {
   const number1 = parseFloat(operand1)
   const number2 = parseFloat(operand2)
