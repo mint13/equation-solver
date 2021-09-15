3ccb1fdb4e23ab63648d863f37e56d4edc2a7c67
Mint Medillo <m.medillo13@gmail.com>
2021-09-15 19:11:04 +0800

    add form submit listener

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ const PARENTHESIS_REGEX = /\((?<equation>[^\(\)]*)\)/
 const MULTIPLY_DIVIDE_REGEX = /(?<operand1>\S+)\s*(?<operation>[\/\*])\s*(?<operand2>\S+)/
 const EXPONENT_REGEX = /(?<operand1>\S+)\s*(?<operation>\^)\s*(?<operand2>\S+)/
 const ADD_SUBTRACT_REGEX = /(?<operand1>\S+)\s*(?<operation>(?<!e)[\-\+])\s*(?<operand2>\S+)/
+
+form.addEventListener("submit", e => {
+  e.preventDefault()
+
+  const result = parse(inputElement.value)
+  outputElement.textContent = result
+})
