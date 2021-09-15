86b7b6d0b3aea0a142b13f89ff81b27752931679
Mint Medillo <m.medillo13@gmail.com>
2021-09-15 19:10:26 +0800

    declare regex patterns

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 const inputElement = document.getElementById("equation")
 const outputElement = document.getElementById("results")
 const form = document.getElementById("equation-form")
+
+const PARENTHESIS_REGEX = /\((?<equation>[^\(\)]*)\)/
+const MULTIPLY_DIVIDE_REGEX = /(?<operand1>\S+)\s*(?<operation>[\/\*])\s*(?<operand2>\S+)/
+const EXPONENT_REGEX = /(?<operand1>\S+)\s*(?<operation>\^)\s*(?<operand2>\S+)/
+const ADD_SUBTRACT_REGEX = /(?<operand1>\S+)\s*(?<operation>(?<!e)[\-\+])\s*(?<operand2>\S+)/
